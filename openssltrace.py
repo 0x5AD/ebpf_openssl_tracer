68a3129f0705acc00bfd9537d2c65680f506071e
Tristan Barrière <tristan.barriere@etudiant.univ-rennes1.fr>
2022-02-17 12:49:19 +0100

    Add SSL_accept probe + tcp_v4/v6_connect

--- openssltrace.py
+++ openssltrace.py
@@ -1,83 +1,131 @@
 #!/usr/bin/python3
 from bcc import BPF
+from bcc.utils import printb
 from socket import inet_ntop, AF_INET, AF_INET6, gethostbyaddr
 from struct import pack, unpack
 
 prog = """
 #include <linux/ptrace.h>
-#include <net/sock.h>
 #include <linux/socket.h>
+#include <net/sock.h>
+#include <bcc/proto.h>
 
 struct ipv4_data_t {
+    u64 ts_us;
     u32 pid;
     u32 uid;
+    u32 saddr;
     u32 daddr;
+    u64 ip;
+    u16 lport;
     u16 dport;
     char task[TASK_COMM_LEN];
 };
 BPF_PERF_OUTPUT(ipv4_events);
 struct ipv6_data_t {
+    u64 ts_us;
     u32 pid;
     u32 uid;
+    unsigned __int128 saddr;
     unsigned __int128 daddr;
+    u64 ip;
+    u16 lport;
     u16 dport;
     char task[TASK_COMM_LEN];
 };
 BPF_PERF_OUTPUT(ipv6_events);
 
-BPF_HASH(currsock, u32, struct sockaddr *);
+BPF_HASH(currsock, u32, struct sock *);
+BPF_HASH(ipv4_hash, u32, struct ipv4_data_t);
+BPF_HASH(ipv6_hash, u32, struct ipv6_data_t);
 
-int syscall__connect(struct pt_regs *ctx, int sockfd, struct sockaddr *addrp, int addrlen) {
-    struct sockaddr_in addr;
-
-    u32 fd = sockfd;
-    if (addrp->sa_family == AF_INET) {
-        u64 pid_tgid = bpf_get_current_pid_tgid();
-        u32 pid = pid_tgid >> 32;
-        u32 tid = pid_tgid;
-        currsock.update(&pid, &addrp);
-    }
-    
+int trace_connect_entry(struct pt_regs *ctx, struct sock *sk)
+{
+    u32 tid = bpf_get_current_pid_tgid();
+    u32 uid = bpf_get_current_uid_gid();
+    // stash the sock ptr for lookup on return
+    currsock.update(&tid, &sk);
     return 0;
-}
+};
 
-int probe_SSL_connect_enter(struct pt_regs *ctx, void *ssl) {  
+static int trace_connect_return(struct pt_regs *ctx, short ipver)
+{
+    int ret = PT_REGS_RC(ctx);
     u64 pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = pid_tgid >> 32;
     u32 tid = pid_tgid;
-    struct sockaddr **addrpp;
-    
-    addrpp = currsock.lookup(&pid);
-    if (addrpp == NULL)
-        return 0;
-
-    if ((*addrpp)->sa_family == AF_INET) {
-        struct sockaddr_in *addrp_v4;
-        addrp_v4 = (struct sockaddr_in *) *addrpp;
-        if (addrp_v4 == NULL)
-            return 0;
-
-        struct ipv4_data_t data = {.pid = pid};
-        data.uid = bpf_get_current_uid_gid();
-        data.daddr = addrp_v4->sin_addr.s_addr;
-        data.dport = addrp_v4->sin_port;
-        bpf_get_current_comm(&data.task, sizeof(data.task));       
-        ipv4_events.perf_submit(ctx, &data, sizeof(data));
-        currsock.delete(&pid);
+    struct sock **skpp;
+    skpp = currsock.lookup(&tid);
+    if (skpp == 0) {
+        return 0;  // missed entry
     }
-    else if ((*addrpp)->sa_family == AF_INET6) {
-        struct sockaddr_in6 *addrp_v6;
-        addrp_v6 = (struct sockaddr_in6 *) *addrpp;
-        if (addrp_v6 == NULL)
-            return 0;
+    if (ret != 0) {
+        // failed to send SYNC packet, may not have populated
+        // socket __sk_common.{skc_rcv_saddr, ...}
+        currsock.delete(&tid);
+        return 0;
+    }
+    // pull in details
+    struct sock *skp = *skpp;
+    u16 lport = skp->__sk_common.skc_num;
+    u16 dport = skp->__sk_common.skc_dport;
+    
+    if (ipver == 4) {
+        struct ipv4_data_t data4 = {.pid = pid, .ip = ipver};
+        data4.uid = bpf_get_current_uid_gid();
+        data4.ts_us = bpf_ktime_get_ns() / 1000;
+        data4.saddr = skp->__sk_common.skc_rcv_saddr;
+        data4.daddr = skp->__sk_common.skc_daddr;
+        data4.lport = lport;
+        data4.dport = ntohs(dport);
+        bpf_get_current_comm(&data4.task, sizeof(data4.task));
+        ipv4_hash.update(&tid, &data4);
+    } else /* 6 */ {
+        struct ipv6_data_t data6 = {.pid = pid, .ip = ipver};
+        data6.uid = bpf_get_current_uid_gid();
+        data6.ts_us = bpf_ktime_get_ns() / 1000;
+        bpf_probe_read_kernel(&data6.saddr, sizeof(data6.saddr),
+            skp->__sk_common.skc_v6_rcv_saddr.in6_u.u6_addr32);
+        bpf_probe_read_kernel(&data6.daddr, sizeof(data6.daddr),
+            skp->__sk_common.skc_v6_daddr.in6_u.u6_addr32);
+        data6.lport = lport;
+        data6.dport = ntohs(dport);
+        bpf_get_current_comm(&data6.task, sizeof(data6.task));
+        ipv6_hash.update(&tid, &data6);
+    }
+    currsock.delete(&tid);
+    return 0;
+}
 
-        struct ipv6_data_t data = {.pid = pid};
-        data.uid = bpf_get_current_uid_gid();
-        bpf_probe_read_kernel(&data.daddr, sizeof(data.daddr), addrp_v6->sin6_addr.s6_addr);
-        data.dport = addrp_v6->sin6_port;
-        bpf_get_current_comm(&data.task, sizeof(data.task));       
+int trace_connect_v4_return(struct pt_regs *ctx)
+{
+    return trace_connect_return(ctx, 4);
+}
+
+int trace_connect_v6_return(struct pt_regs *ctx)
+{
+    return trace_connect_return(ctx, 6);
+}
+
+int probe_SSL_connect_accept_enter(struct pt_regs *ctx, void *ssl) {  
+    u32 tid = bpf_get_current_pid_tgid();
+    struct ipv4_data_t *ipv4p;
+    struct ipv6_data_t *ipv6p;
+    
+    ipv4p = ipv4_hash.lookup(&tid);
+    if (ipv4p != NULL) {
+        struct ipv4_data_t data;
+        bpf_probe_read_kernel(&data, sizeof(data), ipv4p);
+        ipv4_events.perf_submit(ctx, &data, sizeof(data));
+        ipv4_hash.delete(&tid);
+    }
+
+    ipv6p = ipv6_hash.lookup(&tid);
+    if (ipv6p != NULL) {
+        struct ipv6_data_t data;
+        bpf_probe_read_kernel(&data, sizeof(data), ipv6p);
         ipv6_events.perf_submit(ctx, &data, sizeof(data));
-        currsock.delete(&pid);
+        ipv6_hash.delete(&tid);
     }
     
     return 0;
@@ -85,66 +133,52 @@ int probe_SSL_connect_enter(struct pt_regs *ctx, void *ssl) {
 """
 
 
-def dec_to_ipv6(dec):
-  max_int64 = 0xFFFFFFFFFFFFFFFF
-  return inet_ntop(AF_INET6, pack('!QQ', dec >> 64, dec & max_int64))
-
-
-def dec_to_ipv4(dec):
-  return inet_ntop(AF_INET, pack('I', dec))
-
-
-def dec_to_port(dec):
-  return unpack('!H', pack('H', dec))[0]
-
-
-def addr_to_domain_name(addr):
-  return gethostbyaddr(addr)[0]
-
-
 def print_ipv4_events(cpu, data, size):
-    print_ipv6_events(cpu, data, size, AF_INET)
+    event = b["ipv4_events"].event(data)
+    dest_ip = inet_ntop(AF_INET, pack("I", event.daddr)).encode()
+    if event.dport == 443:
+        printb(b"%-6d %-12.12s %-2d %-39s %-6d %-39s %-6d" % \
+               (event.pid, event.task, event.ip,
+                inet_ntop(AF_INET, pack("I", event.saddr)).encode(),
+                event.lport, dest_ip, event.dport))
 
 
 def print_ipv6_events(cpu, data, size):
-    print_ipv6_events(cpu, data, size, AF_INET6)
+    event = b["ipv6_events"].event(data)
+    dest_ip = inet_ntop(AF_INET6, event.daddr).encode()
+    if event.dport == 443:
+        printb(b"%-6d %-12.12s %-2d %-39s %-6d %-39s %-6d" % \
+               (event.pid, event.task, event.ip,
+                inet_ntop(AF_INET6, event.saddr).encode(), event.lport,
+                dest_ip, event.dport))
 
 
-def print_ipv6_events(cpu, data, size, ipver):
-    if (ipver == AF_INET):
-        event = b["ipv4_events"].event(data)
-        addr = dec_to_ipv4(event.daddr)
-    elif (ipver == AF_INET6):
-        event = b["ipv6_events"].event(data)
-        addr = dec_to_ipv6(event.daddr)
-    else:
-        raise "IP version error"
+if __name__ == "__main__":
+    # Initialize BPF
+    b = BPF(text=prog)
     
-    task = event.task.decode()
-    port = dec_to_port(event.dport)
-    if (port == 443):
-        try:
-            domain_name = addr_to_domain_name(addr)
-        except:
-            domain_name = ""
-        print("%-5s %-9s %s:%s %s" % (event.pid, task, addr, port, domain_name))
+    # Attach user and kernel probes to the functions that we want to trace
+    b.attach_kprobe(event="tcp_v4_connect", fn_name="trace_connect_entry")
+    b.attach_kprobe(event="tcp_v6_connect", fn_name="trace_connect_entry")
 
+    b.attach_kretprobe(event="tcp_v4_connect", fn_name="trace_connect_v4_return")
+    b.attach_kretprobe(event="tcp_v6_connect", fn_name="trace_connect_v6_return")
 
-def main():
-    print("%-5s %-9s %-21s" % ("PID", "COMM", "DST"))
+    b.attach_uprobe(name="ssl", sym="SSL_connect", fn_name="probe_SSL_connect_accept_enter")
+    b.attach_uprobe(name="ssl", sym="SSL_accept", fn_name="probe_SSL_connect_accept_enter")
     
+    # Associate the Python print_ipv*_events functions with the ipv*_events streams
+    b["ipv4_events"].open_perf_buffer(print_ipv4_events)
+    b["ipv6_events"].open_perf_buffer(print_ipv6_events)
+
+    # Program Header 
+    print("Hit Ctrl-C to exit")
+    print("%-6s %-12.12s %-2s %-39s %-6s %-39s %-6s" % \
+          ("PID", "COMM", "IP", "SADDR", "LPORT", "DADDR", "DPORT"))
+
+    # Main loop
     while 1:
         try:
             b.perf_buffer_poll()
         except KeyboardInterrupt:
             exit()
-
-
-if __name__ == "__main__":
-    global b
-    b = BPF(text=prog)
-    b.attach_uprobe(name="ssl", sym="SSL_connect", fn_name="probe_SSL_connect_enter")
-    b.attach_kprobe(event=b.get_syscall_fnname("connect"), fn_name="syscall__connect")
-    b["ipv4_events"].open_perf_buffer(print_ipv4_events)
-    b["ipv6_events"].open_perf_buffer(print_ipv6_events)
-    main()
